55c34491d1deee2a904e6358173c91a9a32031b8
jose Antonio rodriguez leandro <alu0100696691@ull.edu.es>
2015-04-20 19:31:37 +0100

    probando test

--- tests/UsuariosTest.php
+++ tests/UsuariosTest.php
@@ -1,4 +1,8 @@
-<?php
+<?php 
+
+use App\Http\Controllers\Controller;
+use App\Models\Usuario;
+
 
 class UsuariosTest extends TestCase {
 
@@ -9,8 +13,10 @@ class UsuariosTest extends TestCase {
 	 */
 	public function testBasicExample()
 	{
-		$response = $this->call('GET', 'login');        	
-		$this->assertFalse($response->isOk());
+		
+		$response = $this->action('GET', 'HomeController@index');		
+		$this->assertEquals(302, $response->getStatusCode());
+					
 	}
 
 }
