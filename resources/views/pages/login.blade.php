8a039a276bf14e69d6a3562ddcdc3358ca5fc0bc
jose Antonio rodriguez leandro <alu0100696691@ull.edu.es>
2015-05-03 09:02:11 +0100

    vista login

--- resources/views/pages/login.blade.php
+++ resources/views/pages/login.blade.php
@@ -4,22 +4,22 @@
 
 </head>
 <body>
-{{ Form::open(array('url' => 'login')) }}
-<h1>Login</h1>
-@if(Session::has('error'))
-<div class="alert-box success">
-  <h2>{{ Session::get('error') }}</h2>
-</div>
-@endif
-<div class="controls">
-{{ Form::text('email','',array('id'=>'','class'=>'form-control span6','placeholder' => 'Please Enter your Email')) }}
-<p class="errors">{{$errors->first('email')}}</p>
-</div>
-<div class="controls">
-{{ Form::password('password',array('class'=>'form-control span6', 'placeholder' => 'Please Enter your Password')) }}
-<p class="errors">{{$errors->first('password')}}</p>
-</div>
-<p>{{ Form::submit('Login', array('class'=>'send-btn')) }}</p>
-{{ Form::close() }}
+  {{ Form::open(array('url' => 'login')) }}
+    <h1>Login</h1>
+    @if(Session::has('error'))
+      <div class="alert-box success">
+        <h2>{{ Session::get('error') }}</h2>
+      </div>
+    @endif
+      <div class="controls">
+      {{ Form::text('email','',array('id'=>'','class'=>'form-control span6','placeholder' => 'Please Enter your Email')) }}
+      <p class="errors">{{$errors->first('email')}}</p>
+      </div>
+      <div class="controls">
+      {{ Form::password('password',array('class'=>'form-control span6', 'placeholder' => 'Please Enter your Password')) }}
+      <p class="errors">{{$errors->first('password')}}</p>
+      </div>
+      <p>{{ Form::submit('Login', array('class'=>'send-btn')) }}</p>
+  {{ Form::close() }}
 </body>
 </html>
